169cc9dec55de2f0104ed5dc9099a5f89427251b
Artem <s-art7@bk.ru>
2023-04-29 11:38:53 +0300

    Add 2 tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class BonusServiceTest {
 
     @Test
-    void shouldCalculateForRegisteredAndUnderLimit(){
+    void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
         long amount = 1000;
@@ -17,11 +17,37 @@ public class BonusServiceTest {
     }
 
     @Test
-    void shouldCalculateForRegisteredAndOverLimit(){
+    void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         long amount = 1_000_000;
         boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = false;
         long expected = 500;
 
         long actual = service.calculate(amount, registered);
